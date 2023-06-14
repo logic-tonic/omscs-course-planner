26e1c987b7407851ca4e9ebff75b65b8f4e0dc99
logic-tonic <christinethedeveloper@gmail.com>
2023-06-14 13:05:01 -0700

    Add sorting to tables

--- src/BasicTable.js
+++ src/BasicTable.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./BasicTable.css"
 
 function BasicTable({rows, addToCourseList, showCheckbox }) {
+  const [sortedRows, setSortedRows] = useState(rows)
+  const [currentlySortedBy, setCurrentlySortedBy] = useState("")
+  const [sortDirection, setSortDirection] = useState("ascending")
+
+  useEffect(() => {
+    sortRows("Course")
+  }, [])
+
+  const sortCompareFunction = (a, b, propertyName, direction) => {
+    if (a[propertyName] < b[propertyName]) {
+      return direction == "ascending" ? -1 : 1;
+    }
+    if (a[propertyName] > b[propertyName]) {
+      return direction == "ascending" ? 1 : -1;
+    }
+    return 0;
+  }
+  const sortRows = (sortHeader) => {
+    setCurrentlySortedBy(sortHeader)
+    let propertyName;
+    if (sortHeader === "Course") { propertyName = "name" }
+    else if (sortHeader === "Foundational?") { propertyName = "isFoundational" }
+    else if (sortHeader === "Rating") { propertyName = "rating" }
+    else if (sortHeader === "Difficulty") { propertyName = "difficulty" }
+    else if (sortHeader === "Workload") { propertyName = "workload" }
+    else if (sortHeader === "Reviews") { propertyName = "reviewCount" }
+    let direction;
+    if (sortHeader === currentlySortedBy) { direction = sortDirection === "ascending" ? "descending" : "ascending" }
+    else { direction = "ascending"}
+    setSortedRows(sortedRows.toSorted((a, b) => sortCompareFunction(a, b, propertyName, direction)))
+    setSortDirection(direction)
+  }
+
   const formatNumber = (value) => {
     return Number.isNaN(value) || typeof value === "undefined"
       ? "N/A"
@@ -15,16 +48,28 @@ function BasicTable({rows, addToCourseList, showCheckbox }) {
             <thead>
               <tr>
                 { showCheckbox && <th scope="col">Added to Course Plan</th> }
-                <th scope="col">Course</th>
-                <th scope="col">Foundational?</th>
-                <th scope="col">Rating</th>
-                <th scope="col">Difficulty</th>
-                <th scope="col">Workload</th>
-                <th scope="col">Reviews</th>
+                <th scope="col" onClick={(event) => sortRows(event.target.childNodes[0]?.textContent.trim())}>
+                  Course {currentlySortedBy === "Course" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                </th>
+                <th scope="col" onClick={(event) => sortRows(event.target.childNodes[0]?.textContent.trim())}>
+                  Foundational? {currentlySortedBy === "Foundational?" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                </th>
+                <th scope="col" onClick={(event) => sortRows(event.target.childNodes[0]?.textContent.trim())}>
+                  Rating {currentlySortedBy === "Rating" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                </th>
+                <th scope="col" onClick={(event) => sortRows(event.target.childNodes[0]?.textContent.trim())}>
+                  Difficulty {currentlySortedBy === "Difficulty" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                </th>
+                <th scope="col" onClick={(event) => sortRows(event.target.childNodes[0]?.textContent.trim())}>
+                  Workload {currentlySortedBy === "Workload" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                </th>
+                <th scope="col" onClick={(event) => sortRows(event.target.childNodes[0]?.textContent.trim())}>
+                  Reviews {currentlySortedBy === "Reviews" ? (sortDirection == "ascending" ? <span className="arrow">↑</span> : <span className="arrow">↓</span>) : null}
+                </th>
               </tr>
             </thead>
             <tbody>
-              { rows.map(
+              { sortedRows.map(
                 (
                   {
                     id,
