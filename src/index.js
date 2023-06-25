75a63109678e4450f0daf68d6507b40e3167c027
logic-tonic <christinethedeveloper@gmail.com>
2023-06-25 16:39:29 -0700

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a258afd1c6e74a54bcca1a36926a013a@o4505422794653696.ingest.sentry.io/4505422796816384",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/omscscourseplanner\.com\/net/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
