a18fd2c8e8e8608abe8aa5e7540a75b444235510
logic-tonic <christinethedeveloper@gmail.com>
2023-10-15 11:38:54 +0400

    update link

--- src/Planner.js
+++ src/Planner.js
@@ -70,7 +70,7 @@ function Planner() {
   }
   
   const fetchReviews = () => {
-    fetch("https://www.omscentral.com/_next/data/hT3uy6qt4jltOATVikIXv/index.json")
+    fetch("https://www.omscentral.com/_next/data/kNJnVhzJYttwZdjT7sSLi/index.json")
     .then(response => response.json())
     .then(data => cleanData(data))
     .then(cleanedData => setReviews(cleanedData))
