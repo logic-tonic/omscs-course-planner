22f0e82dcf7ff02cef5fdc6ddb9ccef8b917ddf8
logic-tonic <christinethedeveloper@gmail.com>
2024-04-26 15:45:04 +0400

    Use docs.omscs.rocks to redirect to latest orientation document, instead of hardcoding

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           When you select a course, it appears in your constructed plan at the very bottom of the page.
           After selecting courses, you can check their availabilities at <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRyHrRhH2V52bsYFEtm-8oJDaFOlyGYz6AKXm8WwsthN3fNP3KGkEx7O7D9ZHV3j2iKnzU2XHqoh4pQ/pubhtml" target="_blank" rel="noreferrer">omscs.rocks</a>.</p>
 
-          Check your orientation document for the most up-to-date information! For example, this is the <a href="https://omscs.gatech.edu/sites/default/files/documents/Other_docs/spring_2024_omscs_orientation_document.pdf" target="_blank" rel="noreferrer">Spring 2024 Orientation Document</a>.
+          Check your orientation document for the most up-to-date information! For example, this is the <a href="http://doc.omscs.rocks/" target="_blank" rel="noreferrer">latest orientation document</a>.
           There are also degree worksheets provided <a href="https://www.cc.gatech.edu/graduate-forms-procedures" target="_blank" rel="noreferrer">here</a> and <a href="https://degreeaudit.gatech.edu/" target="_blank" rel="noreferrer">here</a>. 
           <h2>Degree requirements:</h2>
           <ul>
